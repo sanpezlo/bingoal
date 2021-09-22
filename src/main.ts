e8413a1708d4d7f18cd14a67fb1208e07aa748a0
Santiago Lopez <santiagodavidlopezbenavides@gmail.com>
2021-09-22 11:47:25 -0500

    chore: add nest

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableVersioning();
+  const config = new DocumentBuilder()
+    .setTitle('Bingoal API')
+    .setVersion('0.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
