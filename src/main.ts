d6233e6121b11a22a6f75a19a3cce8ff72c37cc7
Santiago Lopez <santiagodavidlopezbenavides@gmail.com>
2021-09-28 10:58:01 -0500

    chore: add paths

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AppModule } from './app/app.module';
+
+import { AppModule } from '@root/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
