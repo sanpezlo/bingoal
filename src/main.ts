ad527c2199f06137ed611d9b0fe2f900153f310c
Santiago Lopez <santiagodavidlopezbenavides@gmail.com>
2021-09-28 11:55:28 -0500

    chore: add port loader

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
@@ -5,6 +6,8 @@ import { AppModule } from '@root/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
   app.setGlobalPrefix('api');
   app.enableVersioning();
   const config = new DocumentBuilder()
@@ -14,6 +17,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(configService.get<number>('port'));
 }
 bootstrap();
