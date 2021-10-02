9f463441e23c009d7c1d2be2f8bd0a11298c2b7e
Santiago Lopez <santiagodavidlopezbenavides@gmail.com>
2021-10-02 15:11:08 -0500

    refactor: add aliases to paths

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { User, UserSchema } from '@root/users/schemas/user.schema';
-import { UsersRepository } from './users.repository';
+import { UsersRepository } from '@root/users/users.repository';
 
 @Module({
   imports: [
--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
-import { User, UserDocument } from './schemas/user.schema';
+import { User, UserDocument } from '@root/users/schemas/user.schema';
 
 @Injectable()
 export class UsersRepository {
