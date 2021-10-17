85db97f9565712f27cc065c8ac362cd5a4dd1741
Santiago Lopez <santiagodavidlopezbenavides@gmail.com>
2021-10-17 11:14:25 -0500

    feat: add profile route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,9 @@
-import { Version, Post, Body } from '@nestjs/common';
+import { Version, Post, Body, Get } from '@nestjs/common';
 
 import { Controller } from '@root/app/decorators/controller.decorator';
 import { AuthService } from '@root/auth/auth.service';
 import { LoginDto, RefreshDto } from '@root/auth/dto/auth.dto';
-import { Login } from '@root/app/decorators/auth.decorator';
+import { Auth, Login } from '@root/app/decorators/auth.decorator';
 import { User } from '@root/app/decorators/user.decorator';
 import { IUser } from '@root/users/interfaces/user.interface';
 
@@ -22,5 +22,12 @@ export class AuthController {
   @Post('refresh')
   async refresh(@Body() refreshDto: RefreshDto) {
     return this.authService.refresh(refreshDto.refresh);
+  }
+
+  @Version('1')
+  @Auth()
+  @Get('profile')
+  profile(@User() user: IUser) {
+    return user;
   }
 }
